f0af0f5584e1bad3fcc387995e9c9741546760be
zwwill <zwwill7@163.com>
2018-11-30 20:05:09 +0800

    fix: remove noDealPx

--- test.js
+++ test.js
@@ -8,22 +8,5 @@ var fs = require('fs');
   var srcPath = path.join(__dirname, 'test/assets/test.2x.css');
   var srcText = fs.readFileSync(srcPath, {encoding: 'utf8'});
 
-    var outputText = px2rpxIns.generaterpx(srcText);
+  px2rpxIns.generateRpx(srcText);
 
-  // it('should output right @1x file', function () {
-  //   var expectedPath = path.join(__dirname, 'output/default.1x.css');
-  //   var outputText = px2rpxIns.generateThree(srcText, 1);
-  //   assert.equal(outputText, fs.readFileSync(expectedPath, {encoding: 'utf8'}));
-  // });
-
-  // it('should output right @2x file', function () {
-  //   var expectedPath = path.join(__dirname, 'output/default.2x.css');
-  //   var outputText = px2rpxIns.generateThree(srcText, 2);
-  //   assert.equal(outputText, fs.readFileSync(expectedPath, {encoding: 'utf8'}));
-  // });
-
-  // it('should output right @3x file', function () {
-  //   var expectedPath = path.join(__dirname, 'output/default.3x.css');
-  //   var outputText = px2rpxIns.generateThree(srcText, 3);
-  //   assert.equal(outputText, fs.readFileSync(expectedPath, {encoding: 'utf8'}));
-  // });
