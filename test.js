f4f6e2bd54693f44d9baa5bf25ab4f7ee2525fdd
zwwill <zwwill7@163.com>
2018-12-03 16:28:13 +0800

    fix: clean code

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ var Px2rpx = require('./lib/px2rpx');
 var path = require('path');
 var fs = require('fs');
 
-  var px2rpxIns = new Px2rpx({rpxUnit: 1});
+  var px2rpxIns = new Px2rpx({rpxUnit: 2});
   var srcPath = path.join(__dirname, 'test/assets/test.2x.css');
   var srcText = fs.readFileSync(srcPath, {encoding: 'utf8'});
 
--- test/px2rpx.test.js
+++ test/px2rpx.test.js
@@ -5,8 +5,20 @@ var Px2rpx = require('../lib/px2rpx');
 var path = require('path');
 var fs = require('fs');
 
-describe('should work with @2x origin css file', function () {
-  var px2rpxIns = new Px2rpx(/*{rpxUnit: 75, baseDpr: 2}*/);
+describe('should work with @x375 origin css file', function () {
+  var px2rpxIns = new Px2rpx({rpxUnit: 0.5});
+  var srcPath = path.join(__dirname, 'assets/test.css');
+  var srcText = fs.readFileSync(srcPath, {encoding: 'utf8'});
+
+  it('[default] should output right rpx file', function () {
+    var expectedPath = path.join(__dirname, 'output/default.rpx.css');
+    var outputText = px2rpxIns.generateRpx(srcText);
+    assert.equal(outputText, fs.readFileSync(expectedPath, {encoding: 'utf8'}));
+  });
+});
+
+describe('should work with @x750 origin css file', function () {
+  var px2rpxIns = new Px2rpx(/*{rpxUnit: 1}*/);
   var srcPath = path.join(__dirname, 'assets/test.2x.css');
   var srcText = fs.readFileSync(srcPath, {encoding: 'utf8'});
 
@@ -15,52 +27,16 @@ describe('should work with @2x origin css file', function () {
     var outputText = px2rpxIns.generateRpx(srcText);
     assert.equal(outputText, fs.readFileSync(expectedPath, {encoding: 'utf8'}));
   });
-
-  it('should output right @1x file', function () {
-    var expectedPath = path.join(__dirname, 'output/default.1x.css');
-    var outputText = px2rpxIns.generateThree(srcText, 1);
-    assert.equal(outputText, fs.readFileSync(expectedPath, {encoding: 'utf8'}));
-  });
-
-  it('should output right @2x file', function () {
-    var expectedPath = path.join(__dirname, 'output/default.2x.css');
-    var outputText = px2rpxIns.generateThree(srcText, 2);
-    assert.equal(outputText, fs.readFileSync(expectedPath, {encoding: 'utf8'}));
-  });
-
-  it('should output right @3x file', function () {
-    var expectedPath = path.join(__dirname, 'output/default.3x.css');
-    var outputText = px2rpxIns.generateThree(srcText, 3);
-    assert.equal(outputText, fs.readFileSync(expectedPath, {encoding: 'utf8'}));
-  });
 });
 
-describe('should work with @3x origin css file', function () {
-  var px2rpxIns = new Px2rpx({rpxUnit: 112.5, baseDpr: 3});
+describe('should work with @x1125 origin css file', function () {
+  var px2rpxIns = new Px2rpx({rpxUnit: 1.5});
   var srcPath = path.join(__dirname, 'assets/test.3x.css');
   var srcText = fs.readFileSync(srcPath, {encoding: 'utf8'});
 
   it('[default] should output right rpx file', function () {
     var expectedPath = path.join(__dirname, 'output/default.rpx.css');
     var outputText = px2rpxIns.generateRpx(srcText);
-    assert.equal(outputText, fs.readFileSync(expectedPath, {encoding: 'utf8'}));
-  });
-
-  it('should output right @1x file', function () {
-    var expectedPath = path.join(__dirname, 'output/default.1x.css');
-    var outputText = px2rpxIns.generateThree(srcText, 1);
-    assert.equal(outputText, fs.readFileSync(expectedPath, {encoding: 'utf8'}));
-  });
-
-  it('should output right @2x file', function () {
-    var expectedPath = path.join(__dirname, 'output/default.2x.css');
-    var outputText = px2rpxIns.generateThree(srcText, 2);
-    assert.equal(outputText, fs.readFileSync(expectedPath, {encoding: 'utf8'}));
-  });
-
-  it('should output right @3x file', function () {
-    var expectedPath = path.join(__dirname, 'output/default.3x.css');
-    var outputText = px2rpxIns.generateThree(srcText, 3);
     assert.equal(outputText, fs.readFileSync(expectedPath, {encoding: 'utf8'}));
   });
 });
